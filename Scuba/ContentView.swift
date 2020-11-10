bc52c17e35979ff2e51342cfe00507c2c2aa80ab
Lyes <bk@simplon.zebi>
2020-11-10 11:14:17 +0100

    Initial Commit

---
+++ Scuba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scuba
+//
+//  Created by Lyes BOUKBOUKA on 10/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scuba/ScubaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScubaApp.swift
+//  Scuba
+//
+//  Created by Lyes BOUKBOUKA on 10/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ScubaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
